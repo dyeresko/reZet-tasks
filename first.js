0b48b290cf5192aa412aefa9e5273c5498259758
Denis Yeresko <denis.yeresko@gmail.com>
2021-11-04 10:11:40 +0200

    modified first task

--- first.js
+++ first.js
@@ -1,18 +1,21 @@
 function checkTheCondition(array)
 {
     result = [];
-    for (let i = 0; i <= array.length; i++)
+    while (array.length != 0)
     {
         
         let groupArray = array.slice(0, 3);
         array.shift();
-        if ((groupArray[0] > groupArray[1] && groupArray[1] < groupArray[2]) || (groupArray[0] < groupArray[1] && groupArray[1] > groupArray[2]))
+        if (array.length >= 2)
         {
-            result.push(1);
-        }
-        else
-        {
-            result.push(0);
+            if ((groupArray[0] > groupArray[1] && groupArray[1] < groupArray[2]) || (groupArray[0] < groupArray[1] && groupArray[1] > groupArray[2]))
+            {
+                result.push(1);
+            }
+            else
+            {
+                result.push(0);
+            }
         }
     }
     return result
